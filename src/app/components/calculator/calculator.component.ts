9e091a87f679ce7570f73d1d2c52785b32ff5193
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-18 22:47:21 -0400

    functional calculator

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent {
-  clicked = false;
+  display = '';
+
+  onClick(event: any) {
+    const value = event.currentTarget.name;
+
+    console.log(event.currentTarget.name);
+
+    if (value === '=') {
+      try {
+        this.display = eval(this.display).toString();
+      } catch {
+        this.display = 'Error';
+      }
+    } else if (value === 'C') {
+      this.display = '';
+    } else {
+      this.display = this.display.concat(event.currentTarget.name);
+    }
+  }
 }
